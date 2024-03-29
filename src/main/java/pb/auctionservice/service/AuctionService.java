d06882c1605fb501af2663eb9ddfe36823381794
gtauber <gabri.tauber@gmail.com>
2024-03-29 16:12:14 -0300

    feat: New EndPoint to return all Auctions not in uuid param.

--- src/main/java/pb/auctionservice/service/AuctionService.java
+++ src/main/java/pb/auctionservice/service/AuctionService.java
@@ -2,7 +2,6 @@ package pb.auctionservice.service;
 
 import java.util.List;
 import pb.auctionservice.models.dto.AuctionDto;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public interface AuctionService {
